9fe7a891e842b0b19b50473efb8acc6450a74c3c
Pablocerdaaa <pablo.cerda@alumnos.ucn.cl>
2024-06-19 23:52:39 -0400

    added evaluation urls

--- LosTraicionadosBackend/LosTraicionadosBackend/urls.py
+++ LosTraicionadosBackend/LosTraicionadosBackend/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('worker.urls')),
     path('api/', include('multicompany.urls')),
+    path('api/', include('evaluation.urls'))
 ]
